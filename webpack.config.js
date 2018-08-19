d8482648b2052fab8ef58d429e1489bfbd1358e4
Anthony Curtis <tonydc1997@gmail.com>
2018-08-19 08:34:02 -0700

    Add onto webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,12 @@ module.exports = {
   },
   devServer: {
     contentBase: './dist'
+  },
+  module: {
+    rules: [
+      test: /\.(.js|.jsx)$/,
+      exclude: /node_modules/,
+      use: ['babel-loader']
+    ]
   }
 }
