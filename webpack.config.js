18a36ded13d3eb3d2c76dc5359ee662fae2d2b55
Anthony Curtis <tonydc1997@gmail.com>
2018-08-19 08:47:26 -0700

    Configure eslint loader

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,11 @@ module.exports = {
         test: /\.(.js|.jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader']
+      },
+      {
+        test: /\.(.js|.jsx)$/,
+        exclude: /node_modules/,
+        use: ['eslint-loader']
       }
     ]
   },
