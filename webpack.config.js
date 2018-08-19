ce877dba2dd3c080147b918fc398456a89a0d625
Anthony Curtis <tonydc1997@gmail.com>
2018-08-19 08:46:29 -0700

    Fix small error

--- webpack.config.js
+++ webpack.config.js
@@ -12,9 +12,11 @@ module.exports = {
   },
   module: {
     rules: [
-      test: /\.(.js|.jsx)$/,
-      exclude: /node_modules/,
-      use: ['babel-loader']
+      {
+        test: /\.(.js|.jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
     ]
   },
   resolve: {
